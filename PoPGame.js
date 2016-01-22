6033c2f1586496a0e674da353da7e4d641c5a324
ftommasi <mmasito123@yahoo.com>
2016-01-22 05:59:04 +0000

    Generated first version of title screen

--- PoPGame.js
+++ PoPGame.js
@@ -6,19 +6,40 @@
 //TODO(front-end): implement in webGL
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-var Cwidth = 512;
-var Cheight = 480;
+
 //make sure to adapt this canvas size as necessary
-canvas.width = Cwitdh;
+var Cwidth = 1900;
+var Cheight = 1000;
+canvas.width = Cwidth;
 canvas.height = Cheight;
+document.body.appendChild(canvas);
+  
+
+//ctx.fillStyle = "rgb(0, 0, 0)";
+  
+
 
 var render = function(){
-  ctx.font = "20px Helvetica";
-  ctx.fillText("POP",Cwidth/2,Cheight/2);   
+    ctx.font = "80px Helvetica";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    //ctx.fillText("dev test v420.69", 32, 32);
+    ctx.fillText("POP",Cwidth/8 + 90,Cheight/4); 
+    ctx.fillStyle = 'blue';
+    ctx.beginPath();
+    ctx.arc(500,400,50,0,2*Math.PI,false);
+    ctx.fill();
+    ctx.stroke();
+    
+    ctx.beginPath();
+    ctx.arc(300,230,20,0,2*Math.PI,false);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    
 }
 
 var main = function(){
- requestAnimationFrame(main);
+ //requestAnimationFrame(main);
  render();
 }
 var w = window;
