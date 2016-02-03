2172adb687f16f6816ecabafbc2c07781ce00ab1
ftommasi <mmasito123@yahoo.com>
2016-02-03 01:21:57 +0000

    Improved collision

--- PoPGame.js
+++ PoPGame.js
@@ -5,15 +5,12 @@
 
 //PoPGame
  var then = Date.now(); //global variable used for update time modifier
-<<<<<<< HEAD:FaustoPoPGame.js
+ var characterList = []; //global variable used for collision detection
+  var collision = false; //global collision vairable 
  var spriteList = []; //global variable used for collision detection
   var collision, gravity ; //Testing for collisions on different sides
   collision = false; //global collision vairable 
   gravity = true;
-=======
- var characterList = []; //global variable used for collision detection
-  var collision = false; //global collision vairable 
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
 (function(){ //javascript entry point
   
   //add listeners to grab keyboard input from user
@@ -93,23 +90,14 @@
           }
         }
  //Debug text TODO(front-end): remove when not needed before release
-<<<<<<< HEAD:FaustoPoPGame.js
-  debugText1.setText('1 x: '+ spriteList[0].x + ' y: '+ (spriteList[0].y));
-  debugText2.setText('2 x: '+ ball.y + ' y: '+ ball.max_height);        
-=======
   debugText1.setText('1 x: '+ characterList[0].sprite.x + ' y: '+ (characterList[0].sprite.y));
   debugText2.setText('2 x: '+ characterList[1].sprite.x + ' y: '+ (characterList[1].sprite.y));        
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
   //END Debug
     //get keys and call corresponding function 
     if(38 in keysDown && !collision){ //up
         //HACK 
         //TODO(front-end): fix this collision work around
-<<<<<<< HEAD:FaustoPoPGame.js
-         ball.old_y = ball.y-100;
-=======
         ball.sprite.old_y = ball.sprite.y + 10;
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
         //end HACK
         if(!collision && ball.y > ball.max_height){
              ball.moveY(-2,modifier);
@@ -120,11 +108,7 @@
     if(40  in keysDown && !collision){ //down
          //HACK 
         //TODO(front-end): fix this collision work around
-<<<<<<< HEAD:FaustoPoPGame.js
-        ball.old_y = ball.y + 10;
-=======
         ball.sprite.old_y = ball.sprite.y - 10;
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
         //end HACK
         if(!collision){
             ball.moveY(1,modifier);
@@ -134,11 +118,7 @@
     if (37  in keysDown && !collision){ // left
         //HACK 
         //TODO(front-end): fix this collision work around
-<<<<<<< HEAD:FaustoPoPGame.js
-        ball.old_x = ball.x - 10;
-=======
         ball.sprite.old_x = ball.sprite.x + 10;
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
         //end HACK
         if(!collision){
             ball.moveX(-1,modifier);
@@ -148,18 +128,13 @@
     if(39 in keysDown && !collision){ // right
         //HACK 
         //TODO(front-end): fix this collision work around
-<<<<<<< HEAD:FaustoPoPGame.js
-        ball.old_x = ball.x + 10;
-=======
         ball.sprite.old_x = ball.sprite.x - 10;
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
         //end HACK
         if(!collision){
             ball.moveX(1,modifier);
         }
     }
     
-<<<<<<< HEAD:FaustoPoPGame.js
     if(gravity){
         //Gravity effect
         ball.moveY(1,modifier);
@@ -191,9 +166,6 @@
     }
     /*
     for(var i in spriteList){
-=======
-    for(var i in characterList){
->>>>>>> 4c695f00ff6e5969fa8ce1fbc87a2fa1e44c2073:PoPGame.js
        
          if(collision){
             characterList[i].restorePos();
