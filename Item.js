363fe9e8de3ab5ea4c1e2b606497c212db8222dc
Ubuntu - Mate 64-bit <mmasito123@yahoo.com>
2016-02-16 13:29:29 -0800

    Header and comments

--- Item.js
+++ Item.js
@@ -1,3 +1,8 @@
+/* Author/Contributor: Fausto Tommasi
+*  Date: 2/16/2016
+*  Purpose: item class to be used for weapons and powerups   
+*/
+
 function Item(){
   GameObject.call(this);
   this.damage;
