1ffe76266cbcd9cbb731dc993f28e871745a142c
Ubuntu - Mate 64-bit <mmasito123@yahoo.com>
2016-02-16 13:23:01 -0800

    Reimplemented Item branch'

--- Item.js
+++ Item.js
@@ -1,33 +1,9 @@
-/* Author/Contributors: Fausto Tommasi
-*  Date: 2/14/2016
-*  Purpose: Item class for weapons and power-ups
-*/
-function Item(damage,height,width,worldData){
-    GameObject.call(this);
-    this.damage = damage;
-    this.height = height;
-    this.width = width;
-    this.worldData = worldData;
+function Item(){
+  GameObject.call(this);
+  this.damage;
+  this.width;
+  this.height;
 };
-
-
-Item.prototype.activate = function(){
-    //activate collision and damage (think of swinging a sword)
-    //this.addPhysicsComponent(this.x,this.y,Matter.Bodies.rectangle(this.x,this.y,this.height,this.width));
-    this.MakeRigidBody(this.x,this.y.this.worldData,this.width,this.height);
-    
-    
-    
-};
-
-
-Item.prototype.deactivate = function(){
-    //deactivate collision (idle sword)
-    this.physicsComponent = null;
-    
-}
 
 Item.prototype = GameObject.prototype;
-Item.prototype.contructor = Item;
-
-module.exports.Item = Item;
+Item.prototype.constructor = Item;
